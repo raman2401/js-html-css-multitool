b95b71b7a0847566bd5d02756c779b6bdf9c5dae
raman2401 <kathavate.raman@gmail.com>
2019-08-02 13:17:20 +0530

    css minifier added

--- gulpfile.js
+++ gulpfile.js
@@ -1,49 +1,54 @@
 var gulp = require('gulp');
 var jshint = require('gulp-jshint');
-var uglify = require('gulp-terser');
+var terser = require('gulp-terser');
 var stylish = require('jshint-stylish');
 var pump = require('pump');
 var util = require('gulp-util');
-var changed = require('gulp-changed');
-var critical = require('critical');
+var cleanCSS = require('gulp-clean-css');
+var sourcemaps = require('gulp-sourcemaps');
 
-
-var uglifyjs = require('uglify-js'); // can be a git checkout
-                                     // or another module (such as `uglify-es` for ES6 support)
-var composer = require('gulp-uglify/composer');
-var pump = require('pump');
-
-var minify = composer(uglifyjs, console);
-
-
-
-gulp.task('Jshint-CodeQuality', function() {
-	gulp.src(['./**/*.js', '!./node_modules/**/*.js'])
-	    .pipe(jshint())
-	    .pipe(jshint.reporter(stylish))
+// check syntax errors and warnings
+gulp.task('QC-js', function () {
+	gulp.src(['./**/*.js', '!./node_modules/**/*.js', '!./notify/**/*.js', '!minified-js/**/*.js'])
+		.pipe(jshint())
+		.pipe(jshint.reporter(stylish));
 });
 
 
-gulp.task('Uglify-js', function(callback) {
+//minify javascript files 
+gulp.task('minify-js', function (callback) {
 	pump([
-		gulp.src(['./**/*.js', '!./node_modules/**/*.js']),
-		uglify({mangle: false}).on('error', util.log),
+		gulp.src(['./**/*.js', '!./node_modules/**/*.js', '!./notify/**/*.js', '!minified-js/**/*.js']),
+		terser({ mangle: false }).on('error', util.log),
 		gulp.dest('./minified-js')
-    ],
-    callback
-    );
-});
-
-gulp.task('ChangedUglify-js', function(callback) {
-	pump([
-		gulp.src(['./**/*.js', '!./node_modules/**/*.js']),
-		changed('./minified-js'),
-		uglify({mangle: false}).on('error', util.log),
-		gulp.dest('./minified-js')
-		],
+	],
 		callback
-		);
-})
+	);
+});
 
+// minify javascript files and uglify 
+gulp.task('uglify-js', function (callback) {
+	pump([
+		gulp.src(['./**/*.js', '!./node_modules/**/*.js', '!./notify/**/*.js', '!minified-js/**/*.js']),
+		terser({ mangle: true }).on('error', util.log),
+		gulp.dest('./minified-js')
+	],
+		callback
+	);
+});
 
-
+// minify CSS files 
+gulp.task('minify-css', function (callback) {
+	pump([
+		gulp.src('./**/*.css', '!./node_modules/**/*.css', '!minified-css/**/*.css'),
+		sourcemaps.init(),
+		cleanCSS({ compatibility: 'ie8' }, function (details) {
+			console.log(`${details.name}: ${details.stats.originalSize}`);
+			console.log(`${details.name}: ${details.stats.minifiedSize}`);
+		}),
+		sourcemaps.write('./maps', { addComment: false }),
+		gulp.dest('minified-css')
+	],
+		callback
+	);
+});
